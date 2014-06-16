87795513799c04bc223c84474ef737e9c7538f33
Sanjay Singh <sanjaysingh13@gmail.com>
2014-06-16 12:15:58 +0530

    admin3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :admin?
+  helper_method :guest?
   protected
   def admin?
     Rails.logger.debug "Session password is #{session[:password]}"
     Rails.logger.debug "Stored password is #{ENV["ARP_ADMIN"]}"
   session[:password] == ENV["ARP_ADMIN"]
+  end
+  def guest?
+    session[:password] == ENV["ARP_GUEST"]
     
   end
   def authorize
-  	unless admin?
+  	unless admin?||guest?
   		flash[:error] = "unauthorized access"
   		#render films.index
   		false
